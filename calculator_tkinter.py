8afa67dc508337331019d5a74817c35b9dd73af5
chosimja <chosimja@proton.me>
2023-11-25 07:21:30 +0900

    first commit

---
+++ calculator_tkinter.py
@@ -0,0 +1,118 @@
+from tkinter import *
+
+expression = ""
+
+def press(num):
+    global expression
+
+    expression = expression + str(num)
+
+    equation.set(expression)
+
+def equalpress():
+
+    try:
+
+        global expression
+
+        total = str(eval(expression))
+
+        equation.set(total)
+
+        expression = ""
+
+    except:
+        equation.set(" error ")
+        expression = ""
+
+def clear():
+    global expression
+    expression = ""
+    equation.set("")
+
+if True:
+    gui = Tk()
+
+    gui.configure(background="light green")
+
+    gui.title("Simple Calculator")
+
+    gui.geometry("540x300")
+
+    equation = StringVar()
+
+    expression_field = Entry(gui, textvariable=equation)
+    
+    expression_field.grid(columnspan=4, ipadx=70)
+
+    button1 = Button(gui, text=' 1 ', fg='black', bg='red',
+                    command=lambda: press(1), height=1, width=7)
+    button1.grid(row=2, column=0)
+    
+    button2 = Button(gui, text=' 2 ', fg='black', bg='red',
+                    command=lambda: press(2), height=1, width=7)
+    button2.grid(row=2, column=1)
+    
+    button3 = Button(gui, text=' 3 ', fg='black', bg='red',
+                    command=lambda: press(3), height=1, width=7)
+    button3.grid(row=2, column=2)
+    
+    button4 = Button(gui, text=' 4 ', fg='black', bg='red',
+                    command=lambda: press(4), height=1, width=7)
+    button4.grid(row=3, column=0)
+    
+    button5 = Button(gui, text=' 5 ', fg='black', bg='red',
+                    command=lambda: press(5), height=1, width=7)
+    button5.grid(row=3, column=1)
+    
+    button6 = Button(gui, text=' 6 ', fg='black', bg='red',
+                    command=lambda: press(6), height=1, width=7)
+    button6.grid(row=3, column=2)
+
+    button7 = Button(gui, text=' 7 ', fg='black', bg='red',
+                    command=lambda: press(7), height=1, width=7)
+    button7.grid(row=4, column=0)
+
+    button8 = Button(gui, text=' 8 ', fg='black', bg='red',
+                    command=lambda: press(8), height=1, width=7)
+    button8.grid(row=4, column=1)
+
+    button9 = Button(gui, text=' 9 ', fg='black', bg='red',
+                    command=lambda: press(9), height=1, width=7)
+    button9.grid(row=4, column=2)
+
+    button0 = Button(gui, text=' 0 ', fg='black', bg='red',
+                    command=lambda: press(0), height=1, width=7)
+    button0.grid(row=5, column=0)
+
+    button10 = Button(gui, text=' + ', fg='black', bg='red',
+                    command=lambda: press("+"), height=1, width=7)
+    button10.grid(row=2, column=3)
+
+    button11 = Button(gui, text=' - ', fg='black', bg='red',
+                    command=lambda: press("-"), height=1, width=7)
+    button11.grid(row=3, column=3)
+
+    button12 = Button(gui, text=' * ', fg='black', bg='red',
+                    command=lambda: press("*"), height=1, width=7)
+    button12.grid(row=4, column=3)
+
+    button13 = Button(gui, text=' / ', fg='black', bg='red',
+                    command=lambda: press("/"), height=1, width=7)
+    button13.grid(row=5, column=3)
+
+    button14 = Button(gui, text=' = ', fg='black', bg='red',
+                    command=equalpress, height=1, width=7)
+    button14.grid(row=5, column=2)
+
+    button15 = Button(gui, text='Clear', fg='black', bg='red',
+                    command=clear, height=1, width=7)
+    button15.grid(row=5, column=1)
+
+    button16 = Button(gui, text='.', fg='black', bg='red',
+                    command=lambda: press('.'), height=1, width=7)
+    button16.grid(row=6, column=0)
+
+    gui.mainloop()
+
+
